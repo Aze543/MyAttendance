4771039f6f48e80b8dbc6eac3886c2037d7db1a6
Aze543 <121960248+Aze543@users.noreply.github.com>
2025-03-20 21:50:51 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,3 @@
 const year_today = new Date();
 var year = year_today.getFullYear();
-document.getElementById("year_today").innerHTML = "Copyright &copy; BsCpeE 3-1 QR Scanner " + year;
+document.getElementById("year_today").innerHTML = "Copyright &copy; Skibidi Amogus " + year;
