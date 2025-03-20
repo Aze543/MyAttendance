f322bbf16c2c5d596e327f57a6a1f6277808699a
Aze543 <121960248+Aze543@users.noreply.github.com>
2025-03-20 21:10:38 +0800

    Delete QR SCanner directory

--- QR SCanner/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("visitors").innerHTML = visits;
-
-const year_today = new Date();
-var year = year_today.getFullYear();
-document.getElementById("year_today").innerHTML = "Copyright &copy; BsCpeE 3-1 QR Scanner " + year;
-
-import visitors from "./counts.txt";
